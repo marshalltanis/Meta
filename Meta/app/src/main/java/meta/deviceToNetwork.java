7dd29e028db3774252ea7ed5c4b024e8ec85a250
tanism3 <tanism3@students.wwu.edu>
2017-12-01 11:53:46 -0800

    added d2n queue needs to be added to main thread

---
+++ Meta/app/src/main/java/meta/deviceToNetwork.java
@@ -0,0 +1,42 @@
+package meta;
+
+import android.os.ParcelFileDescriptor;
+import android.util.Log;
+
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class deviceToNetwork implements Runnable {
+    private ConcurrentLinkedQueue<Packet> deviceToNetworkQueue;
+    private FileChannel vpnIn;
+    public deviceToNetwork(ConcurrentLinkedQueue<Packet> d2n, ParcelFileDescriptor vpnIn){
+        this.deviceToNetworkQueue = d2n;
+        this.vpnIn = new FileInputStream(vpnIn.getFileDescriptor()).getChannel();
+    }
+    @Override
+    public void run(){
+        Thread us = Thread.currentThread();
+        int bytesRead = 0;
+        while(true){
+            ByteBuffer newPack = ByteBufferPool.acquire();
+            try{
+                 bytesRead= vpnIn.read(newPack);
+                if(bytesRead > 0){
+                    Packet current = new Packet(newPack);
+                    deviceToNetworkQueue.offer(current);
+                    ByteBufferPool.release(newPack);
+                }
+                else {
+                    us.sleep(10);
+                }
+            } catch (Exception e){
+                Log.w("ERROR", e.toString());
+            }
+
+
+        }
+    }
+}
